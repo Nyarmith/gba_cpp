5cc66c32a62672cbccb74e346c5a7d57114b1277
Sergey Ivanov <powah.serge@gmail.com>
2019-09-02 22:55:12 -0400

    modify cmake for better git module support

--- cmake/devKitPro.cmake
+++ cmake/devKitPro.cmake
@@ -4,17 +4,20 @@ cmake_minimum_required(VERSION 3.10)
 
 set(CMAKE_SYSTEM_NAME Generic)
 
-set(CMAKE_C_COMPILER "arm-none-eabi-gcc")
-set(CMAKE_CXX_COMPILER "arm-none-eabi-g++")
+find_program(CMAKE_C_COMPILER arm-none-eabi-gcc)
+if(NOT CMAKE_C_COMPILER)
+    message(FATAL_ERROR "arm-none-eabi-gcc not found, check your devKitPro environment")
+endif()
 
-set(PKG_CONFIG_EXECUTABLE "arm-none-eabi-pkg-config")
+find_program(CMAKE_CXX_COMPILER arm-none-eabi-gcc)
+if(NOT CMAKE_CXX_COMPILER)
+    message(FATAL_ERROR "arm-none-eabi-g++ not found, check your devKitPro environment")
+endif()
 
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
-
-set(BUILD_SHARED_LIBS OFF CACHE INTERNAL "Shared libs not available" )
+find_program(OBJCOPY_EXE arm-none-eabi-objcopy)
+if(NOT OBJCOPY_EXE)
+    message(FATAL_ERROR "arm-none-eabi-objcopy not found, check your devKitPro environment")
+endif()
 
 # overriding CMAKE_OBJCOPY doesn't work for some reason
 find_program(OBJCOPY_EXE arm-none-eabi-objcopy)
@@ -26,6 +29,16 @@ find_program(GBAFIX_EXE gbafix)
 if(NOT GBAFIX_EXE)
     message(FATAL_ERROR "gbafix not found, check your devKitPro environment")
 endif()
+
+set(PKG_CONFIG_EXECUTABLE "arm-none-eabi-pkg-config")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+set(BUILD_SHARED_LIBS OFF CACHE INTERNAL "Shared libs not available" )
+
 
 function(add_gba_target target_ sources_)
     add_executable(${target_} ${sources_})
