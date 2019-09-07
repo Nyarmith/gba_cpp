344a6f2dba6803ea00beb8d5dd3dc86246b3606d
Sergey Ivanov <powah.serge@gmail.com>
2019-09-06 22:05:51 -0400

    libraries setup for rotating cube demo

--- include/fixed.hh
+++ include/fixed.hh
@@ -1,8 +1,12 @@
+#ifndef __GBA_FIXED_HH__
+#define __GBA_FIXED_HH__
+
 #include <cstdint>
 #include <type_traits>
 #include <iostream>
 #include <string>
 
+namespace gba {
 // I+F should be less than half the number of bits in the underlying type
 // to maintain accuracy in multiplication and division
 template<typename T, unsigned I, unsigned F,
@@ -10,56 +14,78 @@ template<typename T, unsigned I, unsigned F,
 class Fixed
 {
 public:
-    Fixed(){}
+    constexpr Fixed(){}
 
-    Fixed(float f)
+    constexpr explicit Fixed(float f)
     {
         data = f*(1<<(F));
     };
 
-    Fixed(double f) : Fixed(static_cast<float>(f)) {};
+    constexpr explicit Fixed(double f) : Fixed(static_cast<float>(f)) {};
 
-    Fixed(T t)
+    constexpr Fixed(T t)
     {
         data = t<<F;
     };
 
-    Fixed operator+(const Fixed &o) const
+    constexpr Fixed operator+(const Fixed &o) const
     {
         Fixed ret;
         ret.data = data + o.data;
         return ret;
     }
-    Fixed operator-(const Fixed &o) const
+    constexpr Fixed operator-(const Fixed &o) const
     {
         Fixed ret;
         ret.data = data - o.data;
         return ret;
     }
-    Fixed operator*(const Fixed &o) const
+    constexpr Fixed operator*(const Fixed &o) const
     {
         Fixed ret;
         ret.data = (data*o.data)>>F;
         return ret;
     }
-    Fixed operator/(const Fixed &o) const
+    constexpr Fixed operator/(const Fixed &o) const
     {
         Fixed ret;
         ret.data = (data<<F)/o.data;
         return ret;
     }
-    operator float() const
+    constexpr Fixed operator-() const
+    {
+        Fixed ret;
+        ret.data = -data;
+        return -ret;
+    }
+    constexpr operator float() const
     {
         return static_cast<float>(data)/(1<<F);
     }
-    operator T() const
+    constexpr operator T() const
     {
         return data>>F;
     }
-    T round() const
+    constexpr T round() const
     {
         return data+(1<<(F-1))>>F;
     }
+
+    constexpr bool operator<(const Fixed o) const
+    {
+        return data<o.data;
+    }
+
+    constexpr bool operator>(const Fixed o) const
+    {
+        return data>o.data;
+    }
+
+    constexpr bool operator==(const Fixed o) const
+    {
+        return data==o.data;
+    }
+    
     std::string to_string() const
     {
         return std::to_string(static_cast<float>(*this));
@@ -68,3 +94,7 @@ public:
 private:
     T data{0};
 };
+
+} // namespace gba
+
+#endif
